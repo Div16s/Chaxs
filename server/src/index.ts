4183dbd07824912a930d76e84bd793f23d24aaf3
Divyankar Shah <divyankarshah1602@gmail.com>
2025-07-04 10:45:10 +0530

    Updated cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 8000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://chaxs.vercel.app"],
         credentials: true,
     },
     adapter: createAdapter(redis)
